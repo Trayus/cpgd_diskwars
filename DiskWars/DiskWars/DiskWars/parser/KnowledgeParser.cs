34f816c1f453676513556245dc097f32da3f43da
Taggart Ashby <tgashby@calpoly.edu>
2014-04-23 12:24:47 -0700

    Ooooo, regex. Also, my gitignore is useless because the files have already been committed... sigh

---
+++ DiskWars/DiskWars/DiskWars/parser/KnowledgeParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DiskWars.parser
+{
+   static class KnowledgeParser
+   {
+      static string mapPattern = @"^maps/(<mapName>[Arena|Box|Breakout|Ring|WallWorld])";
+      static string timePattern = @"^t = (<timeMS>\d+) (\d+) (\d+) (\d+) (\d+)";
+      static string playerPattern = @"^p(<playerNum>\d) pos \<(<posX>\d+),(<posY>\d+)\> rot (<rot>\d+) disk \<(<diskX>\d+),(<diskY>\d+)\> holding (<hasDisk>[t|f]) returning (<diskReturning>[t|f]) shield (<hasShield>[t|f]) speed (<hasSpeed>[t|f])";
+
+      Regex mapRE = new Regex(mapPattern);
+      Regex timeRE = new Regex(timePattern);
+      Regex playerRE = new Regex(playerPattern);
+
+
+   }
+}
