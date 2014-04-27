ab4d31f3d2c49997e6129dfb2e8b31a0a49531d6
Taggart Ashby <tgashby@calpoly.edu>
2014-04-27 14:56:06 -0700

    More parser work, DWFrame slowly coming together, adding properties as I need them.

--- DiskWars/DiskWars/DiskWars/parser/DWFrame.cs
+++ DiskWars/DiskWars/DiskWars/parser/DWFrame.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace DiskWars.parser
 {
    class DWFrame
    {
-      bool isHead;
-      DWFrame nextFrame;
-      DWFrame lastFrame;
+      public bool isHead { set; get; }
+      public int playersAlive { set; get; }
+      public int timeLeft { set; get; }
+      public Vector2[] playerPositions;
+      public Vector2[] diskPositions;
+
+      DWFrame nextFrame { set; get; }
+      DWFrame lastFrame { set; get; }
 
 
    }
--- DiskWars/DiskWars/DiskWars/parser/KnowledgeParser.cs
+++ DiskWars/DiskWars/DiskWars/parser/KnowledgeParser.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework;
+using System.IO;
 
 namespace DiskWars.parser
 {
    static class KnowledgeParser
    {
       private static string mapPattern = @"^maps/(<mapName>[Arena|Box|Breakout|Ring|WallWorld])";
-      private static string timePattern = @"^t = (<timeMS>\d+) (\d+) (\d+) (\d+) (\d+)";
+      private static string timePattern = @"^t = (<timeMS>\d+) (<p1Score>\d+) (<p2Score>\d+) (<p3Score>\d+) (<p4Score>\d+)";
       private static string playerPattern = @"^p(<playerNum>\d) pos \<(<posX>\d+),(<posY>\d+)\> rot (<rot>\d+) disk \<(<diskX>\d+),(<diskY>\d+)\> holding (<hasDisk>[t|f]) returning (<diskReturning>[t|f]) shield (<hasShield>[t|f]) speed (<hasSpeed>[t|f])";
 
       private static Regex mapRE = new Regex(mapPattern);
@@ -22,26 +23,92 @@ namespace DiskWars.parser
        Dictionary<PositionSelector, Dictionary<PositionSelector, DWFrame>>>> 
         knowledgeTree;
 
-
       private static string currentMap;
       private static int currentPlayersAlive;
       private static PositionSelector currentDiskPositions;
       private static PositionSelector currentPlayerPositions;
+      private static DWFrame currentFrame;
+
+      public static void ParsePlaytestDirectory(string dirName)
+      {
+         string[] filePaths = Directory.GetFiles(dirName);
+
+         foreach (string fileName in filePaths)
+         {
+            ParsePlaytest(fileName);
+         }
+      }
 
       public static void ParsePlaytest(string fileName) 
       {
+         if (File.Exists(fileName))
+         {
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+               string line = "";
+               Match mapMatch = null;
 
+               line = sr.ReadLine();
+
+               if (!mapRE.IsMatch(line))
+               {
+                  throw new Exception("KP Error: Unrecognized map line: " + line);
+               }
+
+               mapMatch = mapRE.Match(line);
+               currentMap = mapMatch.Groups["mapName"].Value;
+
+               while ((line = sr.ReadLine()) != null)
+               {
+                  parseTimeAndScore(line, currentFrame);
+                  
+                  while (sr.Peek() != 't')
+                  {
+                     line = sr.ReadLine();
+
+                     parsePlayerAndDisk(line, currentFrame);
+                  }
+               }
+            }
+         }
       }
 
-      public static void ParsePlaytestDircectory(string dirName) 
+      private static void parseTimeAndScore(string line, DWFrame currentFrame)
       {
+         if (!timeRE.IsMatch(line))
+         {
+            throw new Exception("KP Error: Unrecognized time line: " + line);
+         }
 
+         Match timeMatch = timeRE.Match(line);
+         try
+         {
+            currentFrame.timeLeft = Convert.ToInt32(timeMatch.Groups["timeMS"].Value);
+         }
+         catch (FormatException e)
+         {
+            Console.WriteLine("Input string is not a sequence of digits.");
+         }
+         catch (OverflowException e)
+         {
+            Console.WriteLine("The number cannot fit in an Int32.");
+         }
+      }
+
+      private static void parsePlayerAndDisk(string line, DWFrame currentFrame)
+      {
+         if (!playerRE.IsMatch(line))
+         {
+            throw new Exception("KP Error: Unrecognized player line: " + line);
+         }
+
+         Match playerMatch = playerRE.Match(line);
+
+         // TODO: The rest of this, lol.
       }
 
       // The following functions should never be used independently... 
-      // Only chained together.
-      
-      
+      // Only chained together.      
       /// <summary>
       /// The first of four in the chain of methods to return a list of frames 
       /// for a given set of game conditions. This narrows the focus down by map.
